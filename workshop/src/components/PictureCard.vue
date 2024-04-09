6075693db5356b235d82c4b45db11a3f026ad29b
Mikesukami <mikesu2544@gmail.com>
2024-04-09 15:07:59 +0700

    compoment+img

---
+++ workshop/src/components/PictureCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-card width="300">
+        <v-img src="https://bestfriends.org/sites/default/files/styles/hero_mobile/public/hero-dash/Asana3808_Dashboard_Standard.jpg?h=ebad9ecf&itok=cWevo33k" 
+        width="200" 
+        height="300"/>
+        <v-card-title>
+            แมวน่ารัก
+        </v-card-title>
+    </v-card>
+    <v-card width="300">
+        <v-img src="../assets/images.jpg" 
+        width="200" 
+        height="300"/>
+        <v-card-title>
+            ไก่ย่างดวงจันทร์
+        </v-card-title>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- workshop/src/views/Me.vue
+++ workshop/src/views/Me.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="me">
         <h1>Hello Welcome to Me page</h1>
-        
+        <cardpic/>
     </div>
 </template>
 
 <script>
+import cardpic from '../components/PictureCard.vue' 
 export default {
-
+    components: {
+        cardpic
+    }
 }
 </script>
 
