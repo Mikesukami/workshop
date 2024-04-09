cdcc328312a2af5e41e32b422002beef6fc08981
Mikesukami <mikesu2544@gmail.com>
2024-04-09 17:28:44 +0700

    Add detail more

--- workshop/src/components/PictureCard.vue
+++ workshop/src/components/PictureCard.vue
@@ -1,30 +1,59 @@
 <template>
-  <div>
-    <v-card width="300">
-        <v-img src="https://bestfriends.org/sites/default/files/styles/hero_mobile/public/hero-dash/Asana3808_Dashboard_Standard.jpg?h=ebad9ecf&itok=cWevo33k" 
-        width="200" 
-        height="300"/>
-        <v-card-title>
-            แมวน่ารัก
-        </v-card-title>
-    </v-card>
-    <v-card width="300">
-        <v-img src="../assets/images.jpg" 
-        width="200" 
-        height="300"/>
-        <v-card-title>
-            ไก่ย่างดวงจันทร์
-        </v-card-title>
-    </v-card>
-  </div>
+    <div>
+        <v-container>
+            <v-row>
+                <v-col cols="12" md="12">
+                    <v-card>
+                        <v-img src="../assets/profile.jpeg" aspect-ratio="1" max-height="500"></v-img>
+                        <v-card-title class="headline"><b>{{ profile.name }}</b></v-card-title>
+                        <v-card-subtitle>{{ profile.nickname }}</v-card-subtitle>
+                        <v-card-text>
+                            <p><b>Graduation:</b> {{ profile.graduation }}</p>
+                            <p><b>Education:</b> {{ profile.education }}</p>
+                            <p><b>Hobby:</b> {{ profile.hobby }}</p>
+                            <p><b>Strange:</b> {{ profile.strange }}</p>
+                            <p><b>Weakness:</b> {{ profile.weakness }}</p>
+                            <p v-show="showLikes">
+                                <b>Likes:</b>
+                            <ul>
+                                <li v-for="like in profile.likes" :key="like">{{ like }}</li>
+                            </ul>
+                            </p>
+                            <v-btn color="primary" elevation="3" small @click="toggleLikesVisibility">Click for {{ showLikes ?
+                                'Hide' : 'Show' }} Likes</v-btn>
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            profile: {
+                name: "Patiparn Lekmard",
+                nickname: "Roos",
+                graduation: "Prince of Songkla University",
+                education: "Bachelor's in Computer Science",
+                hobby: "Playing guitar",
+                strange: "I'm a leadership learning fast",
+                weakness: "I want everything to be perfect",
+                likes: ["Guitar", "Natural", "Music", "Programming"]
+
+            },
+            showLikes: false
+        }
+    },
+    methods: {
+        toggleLikesVisibility() {
+            this.showLikes = !this.showLikes; // Toggle the visibility
+        }
+    }
 
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
--- workshop/src/views/Me.vue
+++ workshop/src/views/Me.vue
@@ -1,19 +1,24 @@
 <template>
     <div class="me">
-        <h1>Hello Welcome to Me page</h1>
+        <h1>Profile </h1>
         <cardpic/>
+        
+
     </div>
 </template>
 
 <script>
-import cardpic from '../components/PictureCard.vue' 
+import cardpic from '../components/PictureCard.vue'
 export default {
+    data() {
+        return {
+            
+        }
+    },
     components: {
         cardpic
     }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
